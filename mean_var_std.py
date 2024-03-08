8ba7f4443f71ffde91473c61707c282a3324d125
donatkeszthelyi <149483810+donatkeszthelyi@users.noreply.github.com>
2024-03-08 15:43:04 +0100

    Update mean_var_std.py
    cleaning up

--- mean_var_std.py
+++ mean_var_std.py
@@ -14,45 +14,33 @@ def calculate(list0):
                'min':[],
                'sum':[]}
         
-        mean1ar = np.mean(ar, axis=0)
-        mean1 = mean1ar.tolist()
-        mean2ar = np.mean(ar, axis=1)
-        mean2 = mean2ar.tolist()
+        mean1 = np.mean(ar, axis=0).tolist()
+        mean2 = np.mean(ar, axis=1).tolist()
         meanF = np.mean(list0)
         dic['mean'] = [mean1, mean2, meanF]
         
-        var1ar = np.var(ar, axis=0)
-        var1 = var1ar.tolist()
-        var2ar = np.var(ar, axis=1)
-        var2 = var2ar.tolist()
+        var1 = np.var(ar, axis=0).tolist()
+        var2 = np.var(ar, axis=1).tolist()
         varF = np.var(list0)
         dic['variance'] = [var1, var2, varF]
         
-        std1ar = np.std(ar, axis=0)
-        std1 = std1ar.tolist()
-        std2ar = np.std(ar, axis=1)
-        std2 = std2ar.tolist()
+        std1 = np.std(ar, axis=0).tolist()
+        std2 = np.std(ar, axis=1).tolist()
         stdF = np.std(list0)
         dic['standard deviation'] = [std1, std2, stdF]
         
-        max1ar = np.max(ar, axis=0)
-        max1 = max1ar.tolist()
-        max2ar = np.max(ar, axis=1)
-        max2 = max2ar.tolist()
+        max1 = np.max(ar, axis=0).tolist()
+        max2 = np.max(ar, axis=1).tolist()
         maxF = np.max(list0)
         dic['max'] = [max1, max2, maxF]
         
-        min1ar = np.min(ar, axis=0)
-        min1 = min1ar.tolist()
-        min2ar = np.min(ar, axis=1)
-        min2 = min2ar.tolist()
+        min1 = np.min(ar, axis=0).tolist()
+        min2 = np.min(ar, axis=1).tolist()
         minF = np.min(list0)
         dic['min'] = [min1, min2, minF]
         
-        sum1ar = np.sum(ar, axis=0)
-        sum1 = sum1ar.tolist()
-        sum2ar = np.sum(ar, axis=1)
-        sum2 = sum2ar.tolist()
+        sum1 = np.sum(ar, axis=0).tolist()
+        sum2 = np.sum(ar, axis=1).tolist()
         sumF = np.sum(list0)
         dic['sum'] = [sum1, sum2, sumF]
         
