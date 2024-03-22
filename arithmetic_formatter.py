a5a916a23bcf07b82087fbd88c27a7b36dc8a834
donatkeszthelyi <149483810+donatkeszthelyi@users.noreply.github.com>
2024-03-22 22:06:02 +0100

    Update arithmetic_formatter.py
    final fix of input

--- arithmetic_formatter.py
+++ arithmetic_formatter.py
@@ -88,5 +88,5 @@ def arithmetic_arranger(problems, show_answers=True):
 
     return problems
 
-p = input('Please enter your addition/substraction problems in this form: ("x+y", "a-b")! Here:')
+p = input('Please enter your addition/substraction problems in this form: x + y, a - b! Here:').split(', ')
 print(arithmetic_arranger(p))
